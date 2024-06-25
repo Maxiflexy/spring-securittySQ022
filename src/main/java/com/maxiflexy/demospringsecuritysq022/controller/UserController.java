b48ad48d7b988cee1f23c627a29b647fe204d4eb
Maxiflexy <onyemax247@gmail.com>
2024-06-25 12:42:18 +0100

    security commit

---
+++ src/main/java/com/maxiflexy/demospringsecuritysq022/controller/UserController.java
@@ -0,0 +1,24 @@
+package com.maxiflexy.demospringsecuritysq022.controller;
+
+import com.maxiflexy.demospringsecuritysq022.dto.AuthResponseDto;
+import com.maxiflexy.demospringsecuritysq022.dto.RegistrationDto;
+import com.maxiflexy.demospringsecuritysq022.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/auth")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping("/register-user")
+    public ResponseEntity<AuthResponseDto> registerUser(@RequestBody RegistrationDto registrationDto){
+        return ResponseEntity.ok(userService.registerUser(registrationDto));
+    }
+}
